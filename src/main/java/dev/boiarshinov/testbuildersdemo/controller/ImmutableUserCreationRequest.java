fb2c2d46d677c5f16131e68415a895a8be84aac2
Artem Boiarshinov <artem.borya@gmail.com>
2020-12-07 19:37:43 +0400

    add json property annotations

--- src/main/java/dev/boiarshinov/testbuildersdemo/controller/ImmutableUserCreationRequest.java
+++ src/main/java/dev/boiarshinov/testbuildersdemo/controller/ImmutableUserCreationRequest.java
@@ -1,5 +1,6 @@
 package dev.boiarshinov.testbuildersdemo.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.boiarshinov.testbuildersdemo.service.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +14,22 @@ import javax.validation.constraints.Pattern;
 public class ImmutableUserCreationRequest {
 
     @NotEmpty
+    @JsonProperty("firstName")
     private final String firstName;
 
     @NotEmpty
+    @JsonProperty("secondName")
     private final String secondName;
 
     @NotNull
     @Pattern(regexp = "\\d{10}")
+    @JsonProperty("phone")
     private final String phone;
 
     @NotNull
     @Pattern(regexp = "\\d{12}")
     @ValidInnHash
+    @JsonProperty("inn")
     private final String inn;
 
     public User toEntity() {
